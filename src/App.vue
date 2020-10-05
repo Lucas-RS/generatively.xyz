e48ebc78c4a512abb91bd2f63afaf654dd998ae6
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2020-10-05 19:53:28 +1300

    Created App.vue, modified for some basic styling

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Nav />
+  <router-view/>
+</template>
+
+<script>
+import Nav from './components/Nav'
+export default {
+  name: 'App',
+  components: {
+    Nav
+  }
+}
+</script>
+
+<style lang="sass">
+body 
+  margin: 0
+
+#app 
+  width: 100vw
+  height: 100vh
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  background-color: hsl(220deg, 50%, 90%)
+
+</style>
