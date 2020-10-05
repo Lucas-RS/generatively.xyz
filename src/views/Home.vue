4bb3bf262c6057091ee2bf2c30099e6c2ee3b5b3
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2020-10-05 19:54:43 +1300

    Created About and Home views

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <Plate 
+      src="https://res.cloudinary.com/demo/image/upload/sample.jpg" 
+      title="STROKES OF PAINT ON CANVAS" 
+      href="https://google.com"
+    />
+  </div>
+</template>
+
+<script>
+import Plate from '@/components/Plate.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Plate
+  }
+}
+</script>
